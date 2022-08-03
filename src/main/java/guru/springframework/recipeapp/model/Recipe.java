c03daea8c94f10ab59a2c70c9c2b7435f27309dc
Magyarboss <matej_liptak@hotmail.com>
2022-08-03 13:13:39 +0200

    Create Category Entity and added Bidirectional ManyToMany relationship with Recipe (we also used @JoinTable.. to create only one table and we (@JoinColum-s - name= recipe_id) and @inverseJoinColums = @JoinColumn(name = category_id))

--- src/main/java/guru/springframework/recipeapp/model/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/model/Recipe.java
@@ -27,6 +27,9 @@ public class Recipe {
     private Notes notes;
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
 
     public Long getId() {
@@ -115,5 +118,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
