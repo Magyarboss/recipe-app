114b7342084d0d943786363e184483df72ae6995
Magyarboss <matej_liptak@hotmail.com>
2022-08-08 14:48:50 +0200

    Added IndexControllerTest

--- src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -1,13 +1,18 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.model.Recipe;
 import guru.springframework.recipeapp.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.ui.Model;
 
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -30,17 +35,32 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
-        String returnString = indexController.getIndexPage(model);
-
-
 //        model.addAttribute("recipes", recipeService.getRecipes());
 
 //        when(model.addAttribute("recipes")).thenReturn(returnString);
 
-        assertEquals( "index", returnString);
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
 
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String returnString = indexController.getIndexPage(model);
+
+        //then
+        assertEquals( "index", returnString);
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         verify(recipeService, times(1)).getRecipes();
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
     }
 }
